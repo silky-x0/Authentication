3232965f06a2aee2ca9fa23b9bd0a495522e5e6d
Akhil <10akhil.t@gmail.com>
2025-06-26 21:35:18 +0530

    Added db and route

--- JWT/app.ts
+++ JWT/app.ts
@@ -2,7 +2,11 @@ import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import User from "../models/User";
+import mongoose from "mongoose";
+
 const app = express();
+app.use(express.json());
 
 app.use(cookieParser());
 app.use(
@@ -11,6 +15,10 @@ app.use(
 		credentials: true,
 	})
 );
+
+mongoose.connect("mongodb://127.0.0.1:27017/authdb")
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB connection error:", err));
 
 app.get("/", (req: Request, res: Response) => {
 	let token = jwt.sign({ email: "10akhil.t@gmail.com" }, "Secret_key");
@@ -28,6 +36,19 @@ app.get("/v1", (req: Request, res: Response) => {
 	let data = jwt.verify(req.cookies.token, "Secret_key");
 	console.log(data);
 });
+
+app.post('/register', async (req: Request, res: Response) => {
+  try {
+    const { username, email, password, age } = req.body;
+    const user = new User({ username, email, password, age });
+    await user.save();
+    res.status(201).send("User registered");
+  } catch (err) {
+    console.error("Registration error:", err);
+    res.status(400).send("Registration failed");
+  }
+});
+
 
 app.listen(3000, () => {
 	console.log("Server is running on port 3000");
